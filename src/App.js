99156efe93125dbabdf97724d4258227a961461c
Stephyyy D <stephyyyd.@Stephyyys-MacBook-Air.local>
2019-03-06 19:42:38 -0500

    changed css color/test

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
+import './App.css'
 
 
 class App extends Component {
   render() {
     return (
-      <h1>
+      <h1 className='txt-color'>
       Hello World
       </h1>
     );
