d15c44146abea68cc85812b2e03a018c7adedf2e
sdurandisse3 <stephaniedurandisse@pursuit.org>
2019-03-16 21:46:32 -0400

    fixed on click bug

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import './App.css'
 
+
 const list = [
   {
     id: 1,
@@ -23,6 +24,8 @@ class App extends Component {
 
   state = {
     rappers: [],
+  
+
   };
 
   componentDidMount() {
@@ -30,11 +33,24 @@ class App extends Component {
   }
 
 
-  handleEvent = (e) => console.log('button clicked '+e);
+  handleEvent = (rapperId) => {
+    const updatedVotes = this.state.rappers.map(rapper => {
+      if (rapper.id === rapperId) {
+        return Object.assign({}, rapper, {
+          votes: rapper.votes + 1
+        });
+      } else {
+        return rapper;
+      }
+    });
+    this.setState({
+      rappers: updatedVotes
+    });
+  }
 
   render() {
     return (
-      list.map(rapper => 
+      this.state.rappers.map(rapper => 
       <FRapper key={rapper.id} id={rapper.id} name={rapper.name} onVote={this.handleEvent} votes={rapper.votes}/>)
   )}
 }
@@ -47,7 +63,7 @@ this.props.onVote(this.props.id);
  }
 
   render() {
-    return <div className="fr-style">{this.props.name} <button onClick={this.handleClick} >+</button> {this.props.votes} </div>;
+    return <div className="fr-style"> {this.props.name} <button onClick={this.handleClick} >+</button> {this.props.votes} </div>;
   }
 }
 
